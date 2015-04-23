c3af91eb254f609b22546ba5dfb1638fd9c8ce91
DanielMadrid <daniel.diaz@skygate.pl>
2015-04-23 11:48:42 +0200

    Implanted bootstrap in the project.

--- project3/src/Blog/DaniBlogBundle/Controller/PageController.php
+++ project3/src/Blog/DaniBlogBundle/Controller/PageController.php
@@ -6,8 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
 {
-    public function indexAction($name)
+	//We create some vars which are gonna be use to show the user where he is.
+	//This vars are gonna be title and text.
+	//title is a short name and text a short description.
+	
+
+    public function indexAction()
     {
-        return $this->render('DaniBlogBundle:Page:index.html.twig', array('name' => $name));
+    	$title = "Home";
+    	$text = "Welcome to our Blog";
+
+        return $this->render(
+        	'DaniBlogBundle:Page:index.html.twig'
+        	, array(
+        		'title' => $title
+        		, 'text' => $text
+        	)
+        );
     }
 }
