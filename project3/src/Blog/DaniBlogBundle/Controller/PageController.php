155a6c2c4c55edd3dbc041386c22e8860c913032
DanielMadrid <daniel.diaz@skygate.pl>
2015-04-23 10:29:03 +0200

    Added Bootstrap and Tested some features (include twig in place of render twig).

--- project3/src/Blog/DaniBlogBundle/Controller/PageController.php
+++ project3/src/Blog/DaniBlogBundle/Controller/PageController.php
@@ -4,10 +4,10 @@ namespace Blog\DaniBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+class PageController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('DaniBlogBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DaniBlogBundle:Page:index.html.twig', array('name' => $name));
     }
 }
